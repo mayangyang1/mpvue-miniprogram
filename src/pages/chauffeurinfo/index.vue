5f4cff6de9e2d8e8951648901acefad015f54dd8
mayangyang <135878>
2018-06-21 17:44:29 +0800

    new add voitureinfo and chauffeurinfo module

--- src/pages/chauffeurinfo/index.vue
+++ src/pages/chauffeurinfo/index.vue
@@ -1,6 +1,76 @@
 <template>
-  <div class="container pdb20">
-     
+  <div class="container pdt20">
+    <div class="common-list pdl20">
+      <div class="common-item flex-fs pdr20">
+        <div class="title">姓名</div>
+        <div class="edit-item flex-sb">
+          <input  type="text">
+        </div>
+      </div>
+      <div class="common-item flex-fs pdr20">
+        <div class="title">性别</div>
+        <div class="edit-item flex-sb">
+          <input type="text">
+          <img class="arrow" src="/static/images/arrows.png" alt="">
+        </div>
+      </div>
+      <div class="common-item flex-fs pdr20">
+        <div class="title">身份证号</div>
+        <div class="edit-item flex-sb">
+          <input  type="text">
+        </div>
+      </div>
+      <div class="common-item flex-fs pdr20">
+        <div class="title">驾驶证档案编号</div>
+        <div class="edit-item flex-sb">
+          <input  type="text">
+        </div>
+      </div>
+      <div class="common-item flex-fs pdr20">
+        <div class="title">从业资格证号</div>
+        <div class="edit-item flex-sb">
+          <input type="text">
+        </div>
+      </div>
+    </div>
+    <div class="upload-title flex-fs">身份证</div>
+    <div class="upload-list pd20 flex-fs">
+      <div class="upload-item">
+        <img class="close" src="/static/images/delete.png" alt="">
+        <img class="defaultImg mgb20" src="/static/images/pic.png" alt="">
+        <div class="title">身份证正面</div>
+      </div>
+      <div class="upload-item">
+        <img class="close" src="/static/images/delete.png" alt="">
+        <img class="defaultImg mgb20" src="/static/images/pic.png" alt="">
+        <div class="title">身份证反面</div>
+      </div>
+    </div>
+    <div class="upload-title flex-fs">驾驶证</div>
+    <div class="upload-list pd20 flex-fs">
+      <div class="upload-item">
+        <img class="close" src="/static/images/delete.png" alt="">
+        <img class="defaultImg mgb20" src="/static/images/pic.png" alt="">
+        <div class="title">驾驶证第一联照片</div>
+      </div>
+      <div class="upload-item">
+        <img class="close" src="/static/images/delete.png" alt="">
+        <img class="defaultImg mgb20" src="/static/images/pic.png" alt="">
+        <div class="title">驾驶证第二联照片</div>
+      </div>
+    </div>
+    <div class="upload-title flex-fs">从业资格证</div>
+    <div class="upload-list pd20 flex-fs">
+      <div class="upload-item">
+        <img class="close" src="/static/images/delete.png" alt="">
+        <img class="defaultImg mgb20" src="/static/images/pic.png" alt="">
+        <div class="title">从业资格证照片</div>
+      </div>
+    </div>
+    <div class="warp">
+      <div class="common-btn main-bg-color row">确认保存</div>
+      <div class="common-btn main-bg-color row">提交认证</div>
+    </div>
   </div> 
 </template>
 
@@ -23,11 +93,5 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  width: 100vw;
-  min-height: 100vh;
-  background-color: #f2f2f2;
-  font-size: 28rpx;
-}
-
+@import '../../../static/businessCardDetail.css';
 </style>
--- src/pages/voitureinfo/index.vue
+++ src/pages/voitureinfo/index.vue
@@ -1,6 +1,79 @@
 <template>
-  <div class="container pdb20">
-
+  <div class="container pdt20">
+    <div class="common-list pdl20">
+      <div class="common-item flex-fs pdr20">
+        <div class="title">车牌号</div>
+        <div class="edit-item flex-sb">
+          <input  type="text">
+        </div>
+      </div>
+      <div class="common-item flex-fs pdr20">
+        <div class="title">车型</div>
+        <div class="edit-item flex-sb">
+          <input type="text">
+          <img class="arrow" src="/static/images/arrows.png" alt="">
+        </div>
+      </div>
+      <div class="common-item flex-fs pdr20">
+        <div class="title">车长</div>
+        <div class="edit-item flex-sb">
+          <input  type="text">
+          <div class="unit">米</div>
+        </div>
+      </div>
+      <div class="common-item flex-fs pdr20">
+        <div class="title">载重</div>
+        <div class="edit-item flex-sb">
+          <input  type="text">
+          <div class="unit">吨</div>
+        </div>
+      </div>
+      <div class="common-item flex-fs pdr20">
+        <div class="title">动力类型</div>
+        <div class="edit-item flex-sb">
+          <input type="text">
+           <img class="arrow" src="/static/images/arrows.png" alt="">
+        </div>
+      </div>
+      <div class="common-item flex-fs pdr20">
+        <div class="title">牌照类型</div>
+        <div class="edit-item flex-sb">
+          <input type="text">
+           <img class="arrow" src="/static/images/arrows.png" alt="">
+        </div>
+      </div>
+      <div class="common-item flex-fs pdr20">
+        <div class="title">道路运输证号</div>
+        <div class="edit-item flex-sb">
+          <input type="text">
+        </div>
+      </div>
+    </div>
+    <div class="upload-title flex-fs">行驶证</div>
+    <div class="upload-list pd20 flex-fs">
+      <div class="upload-item">
+        <img class="close" src="/static/images/delete.png" alt="">
+        <img class="defaultImg mgb20" src="/static/images/pic.png" alt="">
+        <div class="title">行驶证第一联</div>
+      </div>
+      <div class="upload-item">
+        <img class="close" src="/static/images/delete.png" alt="">
+        <img class="defaultImg mgb20" src="/static/images/pic.png" alt="">
+        <div class="title">行驶证第二联</div>
+      </div>
+    </div>
+    <div class="upload-title flex-fs">道路运输证</div>
+    <div class="upload-list pd20 flex-fs">
+      <div class="upload-item">
+        <img class="close" src="/static/images/delete.png" alt="">
+        <img class="defaultImg mgb20" src="/static/images/pic.png" alt="">
+        <div class="title">道路运输证</div>
+      </div>
+    </div>
+    <div class="warp">
+      <div class="common-btn main-bg-color row">确认保存</div>
+      <div class="common-btn main-bg-color row">提交认证</div>
+    </div>
   </div> 
 </template>
 
@@ -10,25 +83,28 @@ export default {
   components: {},
   data() {
     return {
-      
+     
     };
   },
   methods: {
-   
-  },
-  onShow() {
     
   },
-  
+  onShow() {
+   
+  },
 };
 </script>
 
 <style scoped>
-.container {
-  width: 100vw;
-  min-height: 100vh;
-  background-color: #f2f2f2;
-  font-size: 28rpx;
+@import '../../../static/businessCardDetail.css';
+.common-item:nth-child(5) {
+  border-bottom:1px solid #d8d8d8;
 }
-
+.common-item:nth-child(7) {
+  border-bottom: none;
+}
+.edit-item .unit{
+  font-size: 34rpx;
+  color: #666;
+}
 </style>
