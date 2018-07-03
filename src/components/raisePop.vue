733523fbd85532104cccc109035d242c1aa06681
mayangyang <135878>
2018-07-03 18:07:38 +0800

    update reaiseCash modules

--- src/components/raisePop.vue
+++ src/components/raisePop.vue
@@ -2,33 +2,109 @@
     <div class="raiseCash">
        <div class="over"></div>
        <div class="raiseCashCon">
-        <div class="close"><img mode="widthFix" src="/static/images/close.png" alt=""></div>
-        <div class="title">提现金额</div>
-        <div class="title main-color">¥0.00</div>
-        <div class="tips">额外扣除¥0手续费</div>
+           <div class="content">
+               <div class="close flex-fs"><img @click="close" mode="widthFix" src="/static/images/close.png" alt=""></div>
+                <div class="title">提现金额</div>
+                <div class="titles main-color">¥0.00</div>
+                <div class="tips">额外扣除¥0手续费</div>
+           </div>
+            <div class="inputPassword">
+                <div class="title-tips pdl20">请输入交易密码</div>  
+                <div class='pay-password flex-fs'>
+                    <block  v-for="item in Length" :key="item">
+                    <input class="row" @click='tapFocus' :value="passwardValue.length>=index+1?passwardValue[index]:''" disabled password='true'>
+                    </block>
+                </div>
+                <input  class='ipt'  maxlength="6"  type='number' :value='passwardValue' @input='bindKeyValue' :focus='isFocus' password='true'>
+            </div>
        </div>
-       <div class="inputPassword">
-           <div class="title-tips">请输入交易密码</div>
-           
-       </div>
+       
     </div>
 </template>
 <script>
 export default {
     data(){
         return {
-            isSelectAddress: false
+            Length: 6,
+            passwardValue: '',
         }
     },
     methods:{
-        bindSearch() {
-            this.isSelectAddress = true;
-            this.$emit('searchBar', this.isSelectAddress);
+        close() {
+            this.$emit('isClose');
         }
     }
 }
 </script>
 <style scoped>
+    .raiseCashCon{
+        position:fixed;
+        background-color:#fff;
+        width:90vw;
+        top:100rpx;
+        left:5vw;
+        z-index:3;
+        font-size:30rpx;
+        color:#454545;
+        border-radius:10rpx;
+    }
+    .close{
+        font-size:34rpx;
+        font-weight:600;
+        display:flex;
+        justify-content:flex-start;
+        align-items:center;
 
+    }
+    .close img{
+        width: 35rpx;
+        height: 35rpx;
+    }
+    .content{
+        border-bottom: 1px solid #f2f2f2;
+        padding: 20rpx;
+    }
+    .content .title{
+        font-size: 32rpx;
+        text-align: center;
+        padding-top: 30rpx;
+    }
+    .content .titles{
+        font-size: 46rpx;
+        font-weight: 600;
+        text-align: center;
+        line-height: 80rpx;
+    }
+    .content .tips{
+        font-size: 28rpx;
+        color: #999;
+        text-align: center;
+    }
+    .pay-password{
+        margin:0 20rpx;
+        border:1px solid #ccc;
+        height:80rpx;
+        margin-top:20rpx;
+
+    }
+    .pay-password input{
+        border-right:1px solid #ccc;
+        width:80rpx;
+        height:80rpx;
+        flex:1;
+    }
+    .pay-password input:last-child{
+        border-right: none;
+    }
+    .ipt{
+        width: 0;
+        height: 0;
+    }
+    .inputPassword{
+        margin-top:30rpx;
+    }
+    .title-tips{
+        font-size: 28rpx;
+    }
 </style>
 
--- src/pages/raiseCash/index.vue
+++ src/pages/raiseCash/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="bank-title pdl20">到账银行卡</div>
     <div class="bank-list">
-      <div class="bank-item flex-sb pdlr20">
+      <div class="bank-item flex-sb pdlr20" @click="bindSelectBankCard">
         <div class="bank-name">兴业银行(6816)</div>
         <img mode="widthFix" src="/static/images/arrows.png" alt="">
       </div>
@@ -15,21 +15,60 @@
       </div>
       <div class="raise-tis flex-fs">当前账户余额4.00元，<div class="all">全部提现</div></div>
     </div>
-    <div class="common-btn row main-bg-color">提现</div>
+    <div class="common-btn row main-bg-color" @click="raiseCash">提现</div>
+    <!-- 提现弹窗 -->
+    <raise-pop @isClose="isClose" v-if="isSureInputPassward"></raise-pop>
+    <!-- 银行卡列表 -->
+    <div class="over" v-if="selectBank" @click="cancleSelect"></div>
+    <div class="bank-list-pop" v-if="selectBank">
+      <div class="title-tips">选择到账银行卡</div>
+      <div class="bank-item-pop flex-sb mgl20 pdlr20" @click="choiceBankcard">
+        <div class="bank-name">建设银行</div>
+        <div class="bank-num flex-fs">
+          <div class="tis">尾号: 2092</div>
+          <img mode="widthFix" class="mgl20" src="/static/images/arrows.png" alt="">
+        </div>
+      </div>
+      <div class="bank-item-pop flex-sb mgl20 pdlr20">
+        <div class="bank-name">建设银行</div>
+        <div class="bank-num flex-fs">
+          <div class="tis">尾号: 2092</div>
+          <img mode="widthFix" class="mgl20" src="/static/images/arrows.png" alt="">
+        </div>
+      </div>
+    </div>
   </div> 
 </template>
 
 <script>
 import * as utils from "../../utils/index.js";
+import RaisePop from '@/components/raisePop';
 export default {
-  components: {},
+  components: {
+    'raise-pop': RaisePop
+  },
   data() {
     return {
-      
+      isSureInputPassward: false,
+      selectBank: false,
     };
   },
   methods: {
-    
+    raiseCash() {
+      this.isSureInputPassward = true;
+    },
+    isClose() {
+      this.isSureInputPassward = false;
+    },
+    bindSelectBankCard() {
+      this.selectBank = true;
+    },
+    choiceBankcard() {
+      this.selectBank = false;
+    },
+    cancleSelect() {
+      this.selectBank = false;
+    }
   },
 
 };
@@ -93,4 +132,32 @@ export default {
   margin: 0 auto;
   margin-top: 200rpx;
 }
+/* 银行卡列表弹窗-- start */
+.bank-list-pop{
+  background-color: #fff;
+  position:fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 3;
+  width: 100vw;
+}
+.title-tips{
+  height: 100rpx;
+  border-bottom: 1px solid #ccc;
+  line-height: 100rpx;
+  padding-left: 20rpx;
+}
+.bank-item-pop{
+  height: 100rpx;
+  border-bottom: 1px solid #ccc;
+
+}
+.bank-item-pop:last-child {
+  border-bottom: none;
+}
+.bank-num img{
+  width: 20rpx;
+  height: 20rpx;
+}
+/* 银行卡列表弹窗-- end */
 </style>
